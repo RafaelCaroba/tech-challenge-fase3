ff6e12621131171db4410ddd3f8c9aeda20173b4
Rafael Caroba de Souza <rafa.caroba.17@gmail.com>
2026-03-06 15:08:42 -0300

    estrutura inicial do projeto

---
+++ agendamento-service/src/test/java/com/caroba/fiap/hospital/agendamento_service/AgendamentoServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.caroba.fiap.hospital.agendamento_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgendamentoServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ history-service/src/test/java/com/caroba/fiap/hospital/history_service/HistoryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.caroba.fiap.hospital.history_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HistoryServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
